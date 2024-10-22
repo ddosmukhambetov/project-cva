b32bf6c587c2eea2c8f141f78aeaa6b8dd611e0c
ddos <ddosmukhambetov@gmail.com>
2024-10-23 01:57:38 +0500

    feature: Added signal to stop the program (ctrl + c) in machine_learning/utils/signals_handler.py

---
+++ machine_learning/utils/signals_handler.py
@@ -0,0 +1,17 @@
+import signal
+import sys
+
+
+def stop_signal_handler(signal, frame) -> None:
+    global stop_signal_received
+    stop_signal_received = True
+    print("Stop signal received (CTRL + C), exiting...")
+    sys.exit(0)
+
+
+stop_signal_received = False
+signal.signal(signal.SIGINT, stop_signal_handler)
+
+
+def register_signals():
+    signal.signal(signal.SIGINT, stop_signal_handler)
