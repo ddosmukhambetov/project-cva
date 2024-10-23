ea6dfd18a39b81a9c9b2de4e21d62382c6396d54
ddos <ddosmukhambetov@gmail.com>
2024-10-23 14:39:12 +0500

    fix: remove sys.exit(0) from stop_signal_handler in machine_learning/utils/signals_handler.py to allow graceful shutdown

--- machine_learning/utils/signals_handler.py
+++ machine_learning/utils/signals_handler.py
@@ -1,12 +1,11 @@
 import signal
-import sys
 
 
 def stop_signal_handler(signal, frame) -> None:
     global stop_signal_received
     stop_signal_received = True
     print("Stop signal received (CTRL + C), exiting...")
-    sys.exit(0)
+    # sys.exit(0)
 
 
 stop_signal_received = False
